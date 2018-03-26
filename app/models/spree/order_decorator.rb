c008ea9657581d01f20f328d495f5fbf06f53f92
Stephane CRIVISIER <stephane@crivisier.fr>
2018-03-26 12:07:41 +0200

    Fix many rubocop offenses

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,7 +6,8 @@ Spree::Order.class_eval do
   def trigger_perform_at_completion_in_promotion_actions
     promotions.select{ |promo| promo.eligible?(self) }.map(&:actions).flatten.each do |action|
       next unless action.respond_to?(:perform_at_completion)
-      performed = action.perform_at_completion({ order: self })
+
+      action.perform_at_completion({ order: self })
     end
   end
 end
